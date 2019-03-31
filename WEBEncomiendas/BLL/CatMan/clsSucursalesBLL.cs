e82b98ae3a6621db3d2a46031f91daab596e27a3
Justin098 <justin.calderon@uamcr.net>
2019-03-31 09:59:12 -0600

    Mantenimiento Sucursales

---
+++ WEBEncomiendas/BLL/CatMan/clsSucursalesBLL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DAL.CatMan;
+using BLL.WSEncomiendaBD;
+
+namespace BLL.CatMan
+{
+    public class clsSucursalesBLL
+    {
+
+        public void Listar(ref clsSucursalesDAL objSucDAL)
+        {
+            try
+            {
+                BDServiceClient objWSBD = new BDServiceClient();
+
+
+                string SSP_Nombre = "sp_Listar_Sucursales_Direccion";
+                string SNombreTabla = "Sucursales";
+                string error = "";
+
+                objSucDAL.DtTabla = objWSBD.ListarDatos(SSP_Nombre, SNombreTabla, ref error);
+
+                if (error == string.Empty && objSucDAL.DtTabla != null)
+                {
+                    objSucDAL.Error = string.Empty;
+                }
+                else
+                {
+                    objSucDAL.Error = error;
+                }
+            }
+            catch (Exception ex)
+            {
+                objSucDAL.Error = ex.Message.ToString();
+            }
+        }
+
+    }
+}
---
+++ WEBEncomiendas/DAL/CatMan/clsSucursalesDAL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DAL.CatMan
+{
+    public class clsSucursalesDAL
+    {
+
+        private DataTable _dtTabla;
+        private string _Error;
+
+        public DataTable DtTabla
+        {
+            get
+            {
+                return _dtTabla;
+            }
+
+            set
+            {
+                _dtTabla = value;
+            }
+        }
+
+        public string Error
+        {
+            get
+            {
+                return _Error;
+            }
+
+            set
+            {
+                _Error = value;
+            }
+        }
+    }
+}
